0444da18d7626ef4e67241d3121d4a4498e95660
Mathias Wagner <germannewsmaker@gmail.com>
2023-11-18 17:42:59 +0100

    Updated the main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {createBrowserRouter} from "react-router-dom";
+import {RouterProvider} from "react-router";
+import "@styles/fonts.sass";
+import "@styles/main.sass";
+import Root from "@/common/layouts/Root";
+import Home from "@/pages/Home";
+import Imprint from "@/pages/Imprint";
+import Privacy from "@/pages/Privacy";
+
+const routes = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root/>,
+        children: [
+            {
+                path: "/",
+                element: <Home/>,
+            },
+            {
+                path: "/imprint",
+                element: <Imprint/>,
+            },
+            {
+                path: "/privacy",
+                element: <Privacy/>
+            },
+            {
+                path: "*",
+                element: <div style={{display: "flex", justifyContent: "center"}}>404 - Seite nicht gefunden</div>,
+            },
+        ],
+    }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-
-  </React.StrictMode>,
+    <React.StrictMode>
+        <RouterProvider router={routes}/>
+    </React.StrictMode>,
 )
