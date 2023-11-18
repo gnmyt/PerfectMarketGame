a57c9c746d3138fe50437ff8f65ef9f0a62efc0f
Mathias Wagner <germannewsmaker@gmail.com>
2023-11-18 23:41:01 +0100

    Implemented the Game & End routes into the main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,6 +10,8 @@ import Imprint from "@/pages/Imprint";
 import Privacy from "@/pages/Privacy";
 import Create from "@/pages/Create";
 import Join from "@/pages/Join";
+import Game from "@/pages/Game";
+import End from "@/pages/End";
 
 const routes = createBrowserRouter([
     {
@@ -21,6 +23,9 @@ const routes = createBrowserRouter([
             {path: "/privacy", element: <Privacy/>},
             {path: "/create", element: <Create/>},
             {path: "/join", element: <Join/>},
+            {path: "/join/:code", element: <Join/>},
+            {path: "/game", element: <Game/>},
+            {path: "/end", element: <End/>},
             {
                 path: "*",
                 element: <div style={{display: "flex", justifyContent: "center"}}>404 - Seite nicht gefunden</div>,
