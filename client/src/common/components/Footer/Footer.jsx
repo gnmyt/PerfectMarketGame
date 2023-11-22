05509ddfda765f230a5277a6403f3b4c1639b265
Mathias Wagner <germannewsmaker@gmail.com>
2023-11-22 12:09:13 +0100

    Integrated the group names into the Footer.jsx

--- client/src/common/components/Footer/Footer.jsx
+++ client/src/common/components/Footer/Footer.jsx
@@ -5,11 +5,13 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faHome, faVolumeHigh, faVolumeMute} from "@fortawesome/free-solid-svg-icons";
 import {MusicContext} from "@/common/contexts/MusicContext.jsx";
 import {useContext} from "react";
+import {GroupContext} from "@/common/contexts/GroupContext.jsx";
 
 export const Footer = () => {
     const location = useLocation();
 
     const {musicEnabled, setMusicEnabled} = useContext(MusicContext);
+    const {groupName} = useContext(GroupContext);
 
     const isLegalPage = location.pathname === "/imprint" || location.pathname === "/privacy" || location.pathname === "/end";
     const isIngame = location.pathname === "/game" || location.pathname === "/end" || location.pathname === "/create";
@@ -17,25 +19,29 @@ export const Footer = () => {
     return (
         <footer>
             <div className={"footer-left" + ((isIngame || location.pathname.startsWith("/join"))
-                && location.pathname !== "/end" ? " ingame" : "")}>
-                <a title="BS2AB" href="https://www.bs2ab.de/" target="_blank" rel="noreferrer" className="glassy footer-info">
+            && location.pathname !== "/end" ? " ingame" : "")}>
+                <a title="BS2AB" href="https://www.bs2ab.de/" target="_blank" rel="noreferrer"
+                   className="glassy footer-info">
                     <img src={Logo} alt="Logo"/>
                     <h2>Erstellt in Zusammenarbeit mit BS2AB</h2>
                 </a>
 
                 {isLegalPage && <Link className="glassy footer-home" to="/">
-                        <FontAwesomeIcon icon={faHome}/>
-                    </Link>}
+                    <FontAwesomeIcon icon={faHome}/>
+                </Link>}
 
-                {isIngame && <div className="glassy footer-home"  onClick={() => setMusicEnabled(!musicEnabled)}>
+                {isIngame && <div className="glassy footer-home" onClick={() => setMusicEnabled(!musicEnabled)}>
                     <FontAwesomeIcon icon={musicEnabled ? faVolumeHigh : faVolumeMute}/>
                 </div>}
             </div>
 
             <div className={"glassy footer-legal" + ((isIngame || location.pathname.startsWith("/join"))
-                && location.pathname !== "/end" ? " ingame" : "")}>
-                <Link to={"/privacy"}>Datenschutz</Link>
-                <Link to={"/imprint"}>Impressum</Link>
+            && location.pathname !== "/end" ? " ingame" : "")}>
+                {groupName !== "" && <p>{groupName}</p>}
+                {groupName === "" && <>
+                    <Link to={"/privacy"}>Datenschutz</Link>
+                    <Link to={"/imprint"}>Impressum</Link>
+                </>}
             </div>
         </footer>
     )
