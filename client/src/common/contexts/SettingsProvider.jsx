e6b9ed19b42d5b3cc4db89253a806441ca9d9c89
Mathias Wagner <germannewsmaker@gmail.com>
2023-11-22 13:42:03 +0100

    Added the demand table to the SettingsProvider.jsx

--- client/src/common/contexts/SettingsProvider.jsx
+++ client/src/common/contexts/SettingsProvider.jsx
@@ -4,14 +4,24 @@ export const SettingsContext = createContext({});
 
 export const SettingsProvider = ({children}) => {
     const [rounds, setRounds] = useState(localStorage.getItem("rounds") || 10);
+    const [demandTable, setDemandTable] = useState(JSON.parse(localStorage.getItem("demandTable")) || {
+        0: 50,
+        1800: 40,
+        2200: 30
+    });
 
     const updateRounds = (newValue) => {
         localStorage.setItem("rounds", newValue);
         setRounds(newValue);
     }
 
+    const updateDemandTable = (newValue) => {
+        localStorage.setItem("demandTable", JSON.stringify(newValue));
+        setDemandTable(newValue);
+    }
+
     return (
-        <SettingsContext.Provider value={{rounds, updateRounds}}>
+        <SettingsContext.Provider value={{rounds, updateRounds, demandTable, updateDemandTable}}>
             {children}
         </SettingsContext.Provider>
     );
